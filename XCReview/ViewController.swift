b85717fea55be90527b93584fadd5c587c2c47c4
KPR7168 <pich_ratanak@ymail.com>
2020-02-26 21:31:43 +1100

    Initial Commit

---
+++ XCReview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCReview
+//
+//  Created by Pichratanak Ky on 26/2/20.
+//  Copyright © 2020 Pichratanak Ky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
